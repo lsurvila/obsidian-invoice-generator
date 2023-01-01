4984fd4633fbf045ed77a99c09eb9c2bb0985af2
Liudas Survila <liudas@perrystreet.com>
2023-01-01 09:44:28 +0100

    clean up

--- main.ts
+++ main.ts
@@ -1,7 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Plugin, PluginSettingTab, Setting } from 'obsidian';
-import { ExampleView, VIEW_TYPE_EXAMPLE } from "./view";
-
-// Remember to rename these classes and interfaces!
+import { App, Modal, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 interface InvoiceGeneratorSettings {
 	setting: string;
@@ -14,63 +11,26 @@ const DEFAULT_SETTINGS: InvoiceGeneratorSettings = {
 export default class InvoiceGenerator extends Plugin {
 	settings: InvoiceGeneratorSettings;
 
-	async onload() {
-		this.registerView(
-      VIEW_TYPE_EXAMPLE,
-      (leaf) => new ExampleView(leaf)
-    );
+	private readonly modal = new SampleModal(this.app);
 
+	async onload() {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Invoice Generator', (evt: MouseEvent) => {
+		this.addRibbonIcon('dice', 'Test Invoice Generator', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			this.activateView();
+			new SampleModal(this.app).open();
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('invoice-generator-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: 'open-sample-modal-simple',
 			name: 'Open sample modal (simple)',
 			callback: () => {
-				new SampleModal(this.app).open();
+				this.modal.open();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new InvoiceGeneratorSettingTab(this.app, this));
 
@@ -85,7 +45,7 @@ export default class InvoiceGenerator extends Plugin {
 	}
 
 	onunload() {
-		this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
+		this.modal.close();
 	}
 
 	async loadSettings() {
@@ -95,19 +55,6 @@ export default class InvoiceGenerator extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
-
-	async activateView() {
-    this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
-
-    await this.app.workspace.getRightLeaf(false).setViewState({
-      type: VIEW_TYPE_EXAMPLE,
-      active: true,
-    });
-
-    this.app.workspace.revealLeaf(
-      this.app.workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE)[0]
-    );
-  }
 }
 
 class SampleModal extends Modal {
@@ -117,7 +64,7 @@ class SampleModal extends Modal {
 
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		contentEl.setText('Invoice Generator');
 	}
 
 	onClose() {
