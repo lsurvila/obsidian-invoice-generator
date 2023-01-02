df3ba9c44c2ef8bc442a381f657b1dd5e6b888cd
Liudas Survila <liudas@perrystreet.com>
2023-01-02 02:56:50 +0100

    mvp

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
-# Invoice Generator
+# Invoice Generator Plugin for Obsidian
 
-A plugin for Obsidian to generate a simple monthly invoice page (UNDER-DEVELOPMENT)
+This plugin allows you to generate montlhy invoices for your long-term clients directly from Obsidian. Simply specify information in plugin settings, and the plugin will generate an invoice in markdown format that you can save and send to your client. The plugin will calculate the invoice amount based on the number of hours worked in the previous month, excluding weekends.
+
+## Installation (TODO)
+
+1. Download the `obsidian-invoice-generator.zip` file from the releases page.
+2. In Obsidian, go to the **Plugins** tab in the **Settings** window.
+3. Click the **Install Plugin** button and select the `obsidian-invoice-generator.zip` file.
+4. Activate the plugin by clicking the **Activate** button.
+
+## Configuration
+
+The following options can be configured in the plugin's settings:
+
+- **Invoice Folder**: The folder where the generated invoices will be saved.
+- **Issuer Name**: Your name or the name of your business. This will appear on the invoice as the issuer.
+- **Issuer Address**: Your address or the address of your business. This will appear on the invoice as the issuer's address.
+- **Issuer Email**: Your email address. This will appear on the invoice as the issuer's contact email.
+- **Hourly Rate**: The rate that you charge for your services, in dollars per hour.
+- **Wiring Instructions**: Instructions for making wire transfer payments of invoices. 
+
+## Features
+
+- Generate invoices in markdown format for easy saving and sending to long-term clients.
+- Customize the issuer information and hourly rate in the plugin's settings.
+- Choose the folder where the generated invoices will be saved.
+- Quickly access the invoice generator from anywhere in Obsidian using the ribbon feature.
+
+## Usage (TODO)
+
+To access generator use ribbon feature. Based on this configuration:
+
+Invoice file `Invoice for December 2022 - John Doe (#0001).md` will be created (which can also be exported as PDF using `Obsidian->File->Export PDF`):
+
+## Support
+
+My first Obsidian Plugin, still working on it. If you have any issues or questions about the Invoice Generator plugin, please don't hesitate to [contact me](mailto:liudas.survila@gmail.com).
--- main.ts
+++ main.ts
@@ -1,27 +1,40 @@
-import { App, Modal, Plugin, Vault } from 'obsidian';
+import { App, Modal, Plugin, Vault, Workspace } from 'obsidian';
 import { InvoiceGeneratorSettingTab } from 'settings';
+import { format, getDaysInMonth, isWeekend } from 'date-fns';
 
 interface InvoiceGeneratorSettings {
   invoiceFolder: string;
+	invoiceNumber: number;
+	issuerName: string;
+	issuerAddress: string;
+	issuerEmail: string;
+	hourlyRate: number;
+	wiringInstructions: string;
 }
 
 const DEFAULT_SETTINGS: Partial<InvoiceGeneratorSettings> = {
   invoiceFolder: "invoices",
+	invoiceNumber: 1,
+	issuerName: "",
+	issuerAddress: "",
+	issuerEmail: "",
+	hourlyRate: 0,
+	wiringInstructions: ""
 };
 
 export default class InvoiceGenerator extends Plugin {
 
 	vault: Vault = this.app.vault;
+	workspace: Workspace = this.app.workspace
 	settings!: InvoiceGeneratorSettings;
 
-
-	private readonly modal = new SampleModal(this.app);
+	modal: SampleModal = new SampleModal(this.app);
 
 	async onload() {
 		await this.loadSettings();
 		this.addSettingTab(new InvoiceGeneratorSettingTab(this.app, this));
 		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Test Invoice Generator', () => {
+		this.addRibbonIcon('dollar-sign', 'Generate Monthly Invoice', () => {
 			// Called when the user clicks the icon.
 			// this.modal.open();
 			this.generateInvoice();
@@ -47,30 +60,54 @@ export default class InvoiceGenerator extends Plugin {
   }
 
 	async generateInvoice() {
+		// Get current invoice number from plugin settings
+		let invoiceNumber = this.settings.invoiceNumber;
+
+		// Get name of invoice issuer
+		let issuerName = this.settings.issuerName;
+
     // Calculate previous month and year
     const { month, year } = this.getPreviousMonth();
+		// Convert month and year to strings
+		const date = format(new Date(year, month), 'MMMM yyyy');
+
+		// The number of hours of service provided in the previous month
+		const workingHours = this.calculateWorkingHours(month, year)
 
     // Generate page path for previous month's invoice
-    const pagePath = this.getInvoicePagePath(month, year);
-
-    // Check if page already exists
-    // const files = this.vault.getMarkdownFiles().map((file) => file.path);
-    // if (files.includes(await pagePath)) {
-    //   console.log(`Invoice for ${month + 1}/${year} already exists!`);
-    //   return;
-    // }
-		if (await this.vault.adapter.exists(await pagePath)) {
-			console.log(`Invoice for ${month + 1}/${year} already exists!`);
-      return;
-		}
+    const pagePath = this.getInvoicePagePath(invoiceNumber, issuerName, date);
 
     // Create page content for invoice
-    const pageContent = this.createInvoiceContent(month, year);
+    const pageContent = this.createInvoiceContent(invoiceNumber, issuerName, date, workingHours);
 
     // Create the page
-    await this.app.vault.create(await pagePath, pageContent);
-    console.log(`Invoice for ${month + 1}/${year} created!`);
+    const page = await this.vault.create(await pagePath, pageContent);
+    console.log(`${pagePath} created!`);
+
+		// Increment invoice number and save it to plugin settings
+    invoiceNumber += 1;
+    this.settings.invoiceNumber = invoiceNumber;
+		await this.saveSettings();
+
+		// Open invoice page in the current Obsidian workspace
+    this.workspace.getLeaf().openFile(page)
   }
+
+	calculateWorkingHours(month: number, year: number): number {
+		const daysInMonth = getDaysInMonth(new Date(year, month)); // The number of days in the previous month
+		let workdays = 0;
+
+		// Loop through each day of the previous month
+		for (let i = 1; i <= daysInMonth; i++) {
+  		const date = new Date(year, month, i);
+  		if (!isWeekend(date)) {
+    		// If the day is not a weekend day, increment the workdays counter
+    		workdays++;
+			}
+  	}
+		const hours = workdays * 8; 
+		return hours;
+	}
 
   getPreviousMonth(): { month: number, year: number } {
     // Get current date
@@ -85,7 +122,7 @@ export default class InvoiceGenerator extends Plugin {
       month = 11;
       year -= 1;
     }
-
+		console.log(month + " " + year)
     return { month, year };
   }
 
@@ -97,25 +134,56 @@ export default class InvoiceGenerator extends Plugin {
     await this.saveData(this.settings);
   }
 
-  async getInvoicePagePath(month: number, year: number): Promise<string> {
-    // Convert month and year to strings, pad month with leading zero if necessary
-    const monthStr = (month + 1).toString().padStart(2, '0');
-    const yearStr = year.toString();
+  async getInvoicePagePath(invoiceNumber: number, issuerName: string, date: string): Promise<string> {
+		const invoiceNumberStr = `${invoiceNumber.toString().padStart(4, "0")}`
+
+		// Generate invoice page file name with invoice number included
+    const invoicePageName = `Invoice for ${date} - ${issuerName} (#${invoiceNumberStr})`;
 
 		// Get invoice folder from plugin settings
 		const invoiceFolder = this.settings.invoiceFolder;
-		const directories = `${invoiceFolder}`.split('/');
-		console.log(directories)
 
 		// Create folder(s)
 		await this.vault.adapter.mkdir(invoiceFolder);
 
     // Return file name with invoice folder prefix
-    return `${invoiceFolder}/Invoice ${yearStr}-${monthStr}.md`;
+    return `${invoiceFolder}/${invoicePageName}.md`;
   }
 
-  createInvoiceContent(_month: number, _year: number): string {
-		return `TODO: Add invoice content here.`;
+  createInvoiceContent(invoiceNumber: number, issuerName: string, date: string, workingHours: number): string {
+		const currentDate = format(new Date(), 'dd-MM-yyyy')
+		const issuerAddress = this.settings.issuerAddress;
+		const issuerEmail = this.settings.issuerEmail;
+		const hourlyRate = this.settings.hourlyRate.toLocaleString("en-US", { style: "currency", currency: "USD" });
+		const amount = (this.settings.hourlyRate * workingHours).toLocaleString("en-US", { style: "currency", currency: "USD" });
+		const wiringInstructions = this.settings.wiringInstructions.split(/\r?\n/);
+		const invoiceContent = `
+# Invoice
+
+Date: ${currentDate}
+Invoice Number: ${invoiceNumber}
+
+${issuerName}
+${issuerAddress}
+${issuerEmail}
+
+------------
+
+| Description                 | Hours           | Rate             | Amount     |
+|-----------------------------|-----------------|------------------|------------|
+| Rendered services (${date}) | ${workingHours} | ${hourlyRate}/hr | ${amount}  |
+|                             |                 |                  |            |
+|                             |                 |                  |            |
+|                             |                 |            Total | ${amount}  |
+
+## Wiring Instructions
+|                       |                               |
+|-----------------------|-------------------------------|
+| Bank Account Number   | ${wiringInstructions[0]}      |
+| ACH, IBAN, SWIFT CODE | ${wiringInstructions[1]}      |
+| Bank Name             | ${wiringInstructions[2]}      |
+`.trim();
+		return invoiceContent;
   }
 
 	onunload() {
